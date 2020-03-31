054b2613662e528ce08374626f4715c13faa32ef
BritishPandaChick <sbartley1080@gmail.com>
2020-03-31 17:58:48 -0400

    Add code to draw function for gradients. Add colors for particles

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ window.onload = function(){
 
     //Lets create some particles now 
     var particle_count = 100;
-    for (var i = 0; i < particle_count.length; i++) {
+    for (var i = 0; i < particle_count; i++) {
         particles.push(new particle_count());
     }
 
@@ -23,18 +23,50 @@ window.onload = function(){
         this.location = {x: W/2, y: H/2};
         //radius range = 10-30 
         this.radius = 10 + Math.random()*20;
+        //life range = 20-30 
+        this.life = 20 + Math.random()*10;
+        this.remaining_life = this.life;
+        //colors 
+        this.r = Math.round(Math.random()*255);
+        this.g = Math.round(Math.random()*255);
+        this.b = Math.round(Math.random()*255);
     }
 
     function draw(){
         //Painting the canvas black
+        //Time for lighting magic
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, W, H);
 
         for (var i = 0; i < particles.length; i++){
             var p = particles[i];
             ctx.beginPath();
-            ctx.fillStyle = "white";
-            
+            //changing opacity according to the life
+            //opacity goes to 0 at the end of the life of a particle 
+            p.opacity = Math.round(p.remaining_life/p.life*100)/100;
+            ctx.fillStyle = "rgba(255, 255, 255," + p.opacity + ")";
+
+            //a gradient instead of white fill
+            var gradient = ctx.createRadialGradient(p.location.x, p.location.y, 0, p.location.x, p.location.y, p.radius);
+            gradient.addColorStop(0, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
+            gradient.addColorStop(0.5, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
+            gradient.addColorStop(1, "rgba(" + p.r + ", " + p.g + ", " + p.b + ",0)");
+            ctx.fillStyle = gradient;
+            ctx.arc(p.location.x, p.location.y, p.radius, Math.PI*2, false);
+            ctx.fill();
+
+            //lets move the particles 
+            p.remaining_life--;
+            p.radius--;
+            p.location.x += p.speed.x;
+            p.location.y += p.speed.y;
+
+            //regenerate particles 
+            if(p.remaining_life < 0 || p.radius < 0){
+                //a brand new particle replacing the dead one 
+                particles[i] = new particle();
+            }
         }
     }
+    setInterval(draw, 33);
 }
